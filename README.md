d4069b7877db7b97e095b95651dc1ef725ac89b9
sergiosob <sobrito@gmail.com>
2021-11-22 13:44:41 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# AngularJava3
---
+++ TestAngular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TestComponent } from './component/test/test.component';
+import { ClientComponent } from './component/client/client.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent,
+    ClientComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
